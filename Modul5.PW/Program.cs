e247c37033f29f0c3ef52253491802164f9a9486
Bauyrzhan <galum.bauka03@gmail.com>
2023-10-24 02:38:53 +0600

    Добавьте файлы проекта.

---
+++ Modul5.PW/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ExceptionHandlingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MethodA();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Исключение было обработано в вызывающем методе: {ex.Message}");
+            }
+        }
+
+        static void MethodA()
+        {
+            try
+            {
+                MethodB();
+            }
+            catch (Exception ex)
+            {
+                // Поднимаем исключение в вызывающий метод
+                throw new Exception("Исключение было поднято в вызывающий метод", ex);
+            }
+        }
+
+        static void MethodB()
+        {
+            // Генерируем исключение
+            throw new InvalidOperationException("Исключение было сгенерировано в вызываемом методе.");
+        }
+    }
+}
